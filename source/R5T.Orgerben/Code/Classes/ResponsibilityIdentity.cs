ea70b7f1394591114b1612639d41f566def20ce4
Zeph Arvanitis <zeph.arvanitis@gmail.com>
2021-04-09 13:18:49 -0500

    Add ResponsibilityIdentity

---
+++ source/R5T.Orgerben/Code/Classes/ResponsibilityIdentity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using R5T.Guide;
+
+
+namespace R5T.Orgerben
+{
+    public class ResponsibilityIdentity : TypedGuid
+    {
+        #region Static
+
+        public static ResponsibilityIdentity New()
+        {
+            var responsibilityIdentity = new ResponsibilityIdentity(Guid.NewGuid());
+            return responsibilityIdentity;
+        }
+
+        public static ResponsibilityIdentity From(Guid value)
+        {
+            var responsibilityIdentity = new ResponsibilityIdentity(value);
+            return responsibilityIdentity;
+        }
+
+        #endregion
+
+
+
+        public ResponsibilityIdentity(Guid value)
+            : base(value)
+        {
+        }
+    }
+}
